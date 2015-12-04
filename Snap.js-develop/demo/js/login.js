be42a85d6d1a97d2ee8e00573712a7c6e95ed35b
Yanis HATTAB <kabyle_a_jamais@hotmail.com>
2015-12-04 17:55:24 -0500

    sign in validation

--- Snap.js-develop/demo/js/login.js
+++ Snap.js-develop/demo/js/login.js
@@ -10,11 +10,45 @@ $(document).ready(function(){
 	});
 
 	$('#signin').on('click', function (e) {
+		//validate signup
+
+
 		$("#username").text($("#userid").val());
 		//send login information
 	});
 
 	$('#confirmsignup').on('click', function (e) {
+		//validate signup
+		$('#signupform').validate({
+        rules: {
+            Email: {
+                required: true,
+                email: true,
+                messages: {
+                	email : "Please enter a valid email"
+                }
+            },
+            newuserid: {
+                required: true
+            },
+            newpassword: {
+                required: true,
+                minlength: 1,
+                maxlength: 8,
+                messages: {
+                	minlength : "The password must have more than 1 character",
+                	maxlength : "The password must have less than 8 character"
+                }
+            },
+            reenterpassword: {
+                required: true,
+                equalTo: "#newpassword",
+                messages: {
+                	equalTo : "Must be the same password"
+                }
+            }
+        }
+    	});
 		//send login information
 	});
 });
