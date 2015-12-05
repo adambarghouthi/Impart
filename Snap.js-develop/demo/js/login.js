5ed7a65d652575cfe6357771d49e708eebb7b23c
Yanis HATTAB <kabyle_a_jamais@hotmail.com>
2015-12-04 20:07:03 -0500

    login/sign up works

--- Snap.js-develop/demo/js/login.js
+++ Snap.js-develop/demo/js/login.js
@@ -4,51 +4,134 @@
 */
 
 $(document).ready(function(){
+	//hide password reset button initially
+	$("#passwordreset").hide();
+
 	$('#showLogin').on('click', function(e) {
-		$('#loginModal').modal('show');
-		//document.getElementById("myModal").aria-hiddene;
+		var currentUser = Parse.User.current();
+		if(currentUser == null) {
+			$('#loginModal').modal('show');
+		}
+		else {
+			//user logs out
+			Parse.User.logOut();
+			//notify user
+			$("#generalModalContent").html("<p><h4>Successfully logged out.</h4></p>");
+			$("#generalModal").modal("show");
+			
+			$("#username").text("Guest");
+			$('#showLogin').text("Login/Sign up");
+			$("#passwordreset").hide();
+		}
 	});
 
-	$('#signin').on('click', function (e) {
-		//validate signup
+	$('#signinform').submit(function () {
+		 //validate sign in
+		processLogin($("#userid").val(),
+				$("#passwordinput").val());
+		return false;
+	
+	});
+	
+	$("#passwordreset").on('click', function(e) {
+		var currentUser = Parse.User.current();
+		var email = currentUser.get("email");
+		Parse.User.requestPasswordReset(email, {
+			  success: function() {
+				//notify user
+				$("#generalModalContent").html("<p><h4>Successfully sent a reset password email!</h4></p>");
+				$("#generalModal").modal("show");
+			  },
+			  error: function(error) {
+			    // Show the error message
+				$("#generalModalContent").html("<p><h4>Error: "+error.message+"</h4></p>");
+				$("#generalModal").modal("show");
+			  }
+		});
 
-
-		$("#username").text($("#userid").val());
-		//send login information
 	});
 
-	$('#confirmsignup').on('click', function (e) {
+	$('#signupform').submit(function () {
 		//validate signup
 		$('#signupform').validate({
         rules: {
             Email: {
                 required: true,
-                email: true,
-                messages: {
-                	email : "Please enter a valid email"
-                }
+                email: true
             },
             newuserid: {
                 required: true
             },
             newpassword: {
                 required: true,
-                minlength: 1,
-                maxlength: 8,
-                messages: {
-                	minlength : "The password must have more than 1 character",
-                	maxlength : "The password must have less than 8 character"
-                }
+                rangelength: [1,8]
             },
             reenterpassword: {
                 required: true,
                 equalTo: "#newpassword",
-                messages: {
-                	equalTo : "Must be the same password"
-                }
             }
-        }
+        },
+        messages: {
+        	Email : "Please enter a valid email",
+        	newpassword : {
+        		rangelength : "The password must have more than 1 and less than 8 characters"
+        	},
+        	reenterpassword : {
+        		equalTo : "Must be the same password"
+        	}
+        },
+
+		submitHandler: function(form, validator) {
+			createUser($("#Email").val(),
+					$("#newuserid").val(),
+					$("#newpassword").val());
+		    
+		}
     	});
-		//send login information
+
+		return false;
 	});
+	
+	
 });
+
+function createUser(email, name, password) {
+	var user = new Parse.User();
+	user.set("username", name);
+	user.set("password", password);
+	user.set("email", email);
+
+	// other fields?
+	var result;
+	user.signUp(null, {
+	  success: function(user) {
+		//notify user
+		$("#generalModalContent").html("<p><h4>Successfully signed up</h4></p>");
+		$("#generalModal").modal("show");
+		$('#loginModal').modal('hide');
+	  },
+	  error: function(user, error) {
+	    // Show the error message somewhere
+		$("#generalModalContent").html("<p><h4>Error: "+error.message+"</h4></p>");
+		$("#generalModal").modal("show");
+	  }
+	});
+}
+
+function processLogin(name, password) {
+	Parse.User.logIn(name, password, {
+		  success: function(user) {
+			  //notify user
+			  $("#generalModalContent").html("<p><h4>Successfully logged in!</h4></p>");
+			  $("#generalModal").modal("show");
+			  $("#username").text(name);
+			  $('#loginModal').modal('hide');
+			  $('#showLogin').text("Log Out");
+			  $("#passwordreset").show();
+		  },
+		  error: function(user, error) {
+			  $("#generalModalContent").html("<p><h4>Error: "+error.message+"</h4></p>");
+			  $("#generalModal").modal("show");
+		  }
+		});
+}
